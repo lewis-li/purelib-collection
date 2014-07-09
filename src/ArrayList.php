5254c35c20d57ae0fe36e9e2b40ded29438237e2
lewis.li <lewis.li@purelib.com>
2014-07-09 12:57:07 +0800

    add method: ArrayList::each($callback)

--- src/ArrayList.php
+++ src/ArrayList.php
@@ -1,26 +1,30 @@
 <?php
+
 namespace PureLib\Collection;
 
 class ArrayList extends \ArrayObject {
     public static function newInstance($arr) {
-        return new self($arr);
+        return new self ( $arr );
     }
-    
     public function map($callback) {
-        $result = new self();
-        foreach ($this as $k=>$v) {
-            $result[$k] = $callback($k, $v);
+        $result = new self ();
+        foreach ( $this as $k => $v ) {
+            $result [$k] = $callback ( $k, $v );
         }
         return $result;
     }
-    
     public function filter($callback) {
-        $result = new self();
-        foreach ($this as $k=>$v) {
-            if ($callback($k, $v)) {
-                $result[$k] = $v;
+        $result = new self ();
+        foreach ( $this as $k => $v ) {
+            if ($callback ( $k, $v )) {
+                $result [$k] = $v;
             }
         }
         return $result;
+    }
+    public function each($callback) {
+        foreach ( $this as $k => $v ) {
+            $callback ( $k, $v );
+        }
     }
 }
