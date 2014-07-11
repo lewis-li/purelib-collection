287bd1b1fc893fe661a7bac0dd6f42654987b5d6
lewis.li <lewis.li@purelib.com>
2014-07-11 14:29:22 +0800

    添加注释

--- src/ArrayList.php
+++ src/ArrayList.php
@@ -2,10 +2,25 @@
 
 namespace PureLib\Collection;
 
+/**
+ * 为数组添加常用方法
+ *
+ */
 class ArrayList extends \ArrayObject {
+    /**
+     * 实例化当前类对象
+     * @param array $arr
+     * @return \PureLib\Collection\ArrayList
+     */
     public static function newInstance($arr) {
         return new self ( $arr );
     }
+    
+    /**
+     * 对数据进行MAP操作，类似array_map函数，
+     * @param callable $callback callback 接受两个参数，第一个参数为当前数组项的KEY值，第二个参数是当前数组项的值
+     * @return \PureLib\Collection\ArrayList
+     */
     public function map($callback) {
         $result = new self ();
         foreach ( $this as $k => $v ) {
@@ -13,6 +28,12 @@ class ArrayList extends \ArrayObject {
         }
         return $result;
     }
+    
+    /**
+     * 对数据进行filter操作，类似array_filter函数，第一个参数为当前数组项的KEY值，第二个参数是当前数组项的值
+     * @param callback $callback
+     * @return Ambigous <\PureLib\Collection\ArrayList, null>
+     */
     public function filter($callback) {
         $result = new self ();
         foreach ( $this as $k => $v ) {
@@ -22,6 +43,12 @@ class ArrayList extends \ArrayObject {
         }
         return $result;
     }
+    
+    /**
+     * 对数据循环执行callback,第一个参数为当前数组项的KEY值，第二个参数是当前数组项的值
+     * @param unknown $callback
+     * @return void
+     */
     public function each($callback) {
         foreach ( $this as $k => $v ) {
             $callback ( $k, $v );
--- src/ArrayTable/ArrayTable.php
+++ src/ArrayTable/ArrayTable.php
@@ -5,7 +5,6 @@ namespace PureLib\Collection\ArrayTable;
 /**
  * 为符合条件的二维或多维数组提供常用的方法.
  * 
- * @author lewis
  * @todo 优化:读取关联字段时,才触发关联数组的分析和合并
  * @todo 考虑使用AppendIterator的可行性
  */
@@ -13,15 +12,31 @@ class ArrayTable {
     protected $schema = array ();
     protected $data;
     protected $relations = array ();
-    public function __construct($data = null, array $schema = null) {
+    
+    /**
+     * @param array $data 数组
+     * @param array $schema 数组定义（pk: 主键）
+     */
+    public function __construct(array $data = null, array $schema = null) {
         $this->data = $data;
         $this->schema = $schema;
     }
     
+    /**
+     * 新建当前类对象
+     * @param string $data
+     * @param array $schema
+     * @return \PureLib\Collection\ArrayTable\ArrayTable
+     */
     public static function newInstance($data=null, array $schema = null) {
         return new self($data, $schema);
     }
     
+    /**
+     * 设置数组主键
+     * @param string $key
+     * @return Ambigous <boolean, multitype:>|\PureLib\Collection\ArrayTable\ArrayTable
+     */
     public function pk($key = null) {
         if ($key === null) {
             return isset ( $this->schema ['pk'] ) ? $this->schema ['pk'] : false;
@@ -30,6 +45,16 @@ class ArrayTable {
             return $this;
         }
     }
+    
+    /**
+     * 添加数组关联
+     * @param array $options
+     * -# name 关联名称，该名称在数组合并时，作为返回数组的一个字段
+     * -# fk 关联字段
+     * -# data 数据
+     * @throws \Exception
+     * @return \PureLib\Collection\ArrayTable\ArrayTable
+     */
     public function relate(array $options) {
         if (! isset ( $options ['name'] ) && ! is_null ( $options ['name'] ) || ! isset ( $options ['fk'] ) && ! is_null ( $options ) || ! isset ( $options ['data'] ) && ! is_null ( $options ['data'] )) {
             throw new \Exception ( '缺少必要选项' );
@@ -42,8 +67,8 @@ class ArrayTable {
     /**
      *
      * @param array $options
-     *            -# callable filter
-     *            -# bool relation
+     *            -# callable filter 对每一行数据进行filter和map操作，返回false时，过滤当前行.接受一个参数,代表数据的当前行
+     *            -# bool relation 是否合并关联数组
      * @throws \Exception
      * @return Ambigous <multitype:, string>
      */
@@ -61,6 +86,11 @@ class ArrayTable {
         
         return $data;
     }
+    /**
+     * 将MASTER和其他关联数据进行合并
+     * @throws \Exception
+     * @return Ambigous <NULL, string>
+     */
     protected function getMasterAndRelationData() {
         if (! isset ( $this->schema ['pk'] )) {
             throw new \Exception ( '必须指定主键' );
